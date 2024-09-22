94c0934845a806d0079ab199306c830e079197ba
orkaboy <orkaboy@gmail.com>
2024-09-22 08:41:21 +0200

    Sequencer test code (WIP)

---
+++ crates/seq/src/lib.rs
@@ -0,0 +1,136 @@
+use std::fmt::Display;
+
+pub trait Node {
+    fn execute(&mut self, _delta: f64) -> bool {
+        true
+    }
+    fn advance_to_checkpoint(&mut self, _checkpoint: &str) -> bool {
+        false
+    }
+}
+
+
+pub struct SeqLog {
+    text: String,
+    // TODO: More log info, like log level?
+}
+
+impl SeqLog {
+    pub fn create(text: &str) -> Box<Self> {
+        Box::new(SeqLog {
+            text: text.to_owned()
+        })
+    }
+}
+
+impl Display for SeqLog {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.text)
+    }
+}
+
+impl Node for SeqLog {
+    fn execute(&mut self, _delta: f64) -> bool {
+        // TODO: better logging lib?
+        println!("SeqLog: {}", self);
+        true
+    }
+}
+
+#[derive(Default)]
+pub struct SeqList {
+    name: String,
+    children: Vec<Box<dyn Node>>,
+    step: usize,
+}
+
+impl SeqList {
+    pub fn create(name: &str, children: Vec<Box<dyn Node>>) -> Box<Self> {
+        Box::new(SeqList {
+            name: name.to_owned(),
+            children,
+            ..Default::default()
+        })
+    }
+}
+
+impl Display for SeqList {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}({}/{})", self.name, self.step + 1, self.children.len())
+    }
+}
+
+impl Node for SeqList {
+    fn execute(&mut self, delta: f64) -> bool {
+        if self.step >= self.children.len() {
+            true
+        } else {
+            if self.children[self.step].execute(delta) {
+                self.step = self.step + 1;
+            }
+            false
+        }
+    }
+
+    fn advance_to_checkpoint(&mut self, checkpoint: &str) -> bool {
+        loop {
+            if self.step >= self.children.len() {
+                return false;
+            }
+            if self.children[self.step].advance_to_checkpoint(checkpoint) {
+                return true;
+            } else {
+                self.step = self.step + 1;
+            }
+        }
+    }
+}
+
+
+#[derive(Default)]
+pub struct SeqCheckpoint {
+    pub checkpoint_name: String,
+}
+
+impl SeqCheckpoint {
+    pub fn create(name: &str) -> Box<Self> {
+        Box::new(SeqCheckpoint {
+            checkpoint_name: name.to_owned()
+        })
+    }
+}
+
+impl Node for SeqCheckpoint {
+    fn advance_to_checkpoint(&mut self, checkpoint: &str) -> bool {
+        self.checkpoint_name == checkpoint
+    }
+}
+
+
+pub struct Sequencer {
+    root: Box<dyn Node>,
+}
+
+impl Sequencer {
+    pub fn create(root: Box<dyn Node>) -> Self {
+        Sequencer {
+            root
+        }
+    }
+}
+
+impl Sequencer {
+    pub fn run(&mut self) {
+        let mut timer = delta::Timer::new();
+        loop {
+            let deltatime = timer.mark_secs();
+            if self.root.execute(deltatime) {
+                break;
+            }
+        }
+    }
+
+    pub fn advance_to_checkpoint(&mut self, checkpoint: &str) -> bool {
+        self.root.advance_to_checkpoint(checkpoint)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 mod state;
 use eframe::egui;
 
+use seq::{Sequencer, SeqLog, SeqList, SeqCheckpoint};
+
+fn seq_test() {
+    // Create a sequencer object
+    let mut sequencer = Sequencer::create(
+        SeqList::create("Root", vec![
+            SeqLog::create("Starting TAS"),
+            SeqLog::create("Selecting character"),
+            SeqLog::create("Mountain Trail 1"),
+            SeqCheckpoint::create("Mooncradle"),
+            SeqList::create("Mooncradle", vec![
+                SeqLog::create("Garl nooo..."),
+                SeqLog::create("Training"),
+                SeqLog::create("Final Trials"),
+            ]),
+            SeqList::create("Forbidden Cave", vec![
+                SeqLog::create("Juking snails"),
+                SeqLog::create("Big boss"),
+                SeqLog::create("Leaving caves"),
+            ]),
+            SeqLog::create("Make jam"),
+        ])
+    );
+
+    // Optionally, advance to a checkpoint in the sequence
+    //sequencer.advance_to_checkpoint("Checkpoint");
+
+    // Run the sequence (grabs control atm)
+    sequencer.run();
+}
+
+
 fn main() {
+
+    seq_test();
+
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([300.0, 300.0])
