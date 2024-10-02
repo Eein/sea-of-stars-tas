582caec2c2a543fdf0741066e0940908ce79244f
William Volin <me@williamvolin.com>
2024-10-02 00:28:23 -0700

    feat: add intital relect selection screen

--- src/gui/helpers/title_helper.rs
+++ src/gui/helpers/title_helper.rs
@@ -12,6 +12,14 @@ impl TitleHelper {
     }
 }
 
+fn relic_button_decorator(enabled: bool, name: &str) -> String {
+    let enabled_string = match enabled {
+        true => "[x]",
+        false => "[ ]"
+    };
+    format!("{} {}", enabled_string, name)
+}
+
 impl GuiHelper for TitleHelper {
     fn draw(&mut self, managers: &MemoryManagers, ui: &mut egui::Ui, _tab: &mut String) {
         ui.label(format!(
@@ -26,5 +34,13 @@ impl GuiHelper for TitleHelper {
             "Load Save Done: {:?}",
             managers.title_sequence_manager.data.load_save_done
         ));
+
+        ui.separator();
+
+        ui.label(format!("Relics (Total: {})", managers.title_sequence_manager.data.relic_buttons.count));
+        for relic in managers.title_sequence_manager.data.relic_buttons.buttons.iter() {
+            ui.label(relic_button_decorator(relic.enabled, &relic.name));
+        }
+
     }
 }
--- src/memory/title_sequence_manager.rs
+++ src/memory/title_sequence_manager.rs
@@ -26,6 +26,8 @@ impl Default for MemoryManager<TitleSequenceManagerData> {
 pub struct TitleSequenceManagerData {
     /// Title Menu Object Data.
     pub title_menu: TitleMenu,
+    // relicSelectionScreen -> relicButtons
+    pub relic_buttons: RelicButtons,
     /// If saves are loaded and continue shows up on the title screen.
     pub load_save_done: bool,
     /// If the player has pressed start on the intro screen.
@@ -45,6 +47,7 @@ impl MemoryManagerUpdate for TitleSequenceManagerData {
                         self.update_title_menu(class, process, module, singleton)?;
                         self.update_pressed_start(class, process, module, singleton)?;
                         self.update_load_save_done(class, process, module, singleton)?;
+                        self.update_relics(class, process, module, singleton)?;
                     }
                 }
             }
@@ -180,6 +183,67 @@ impl TitleSequenceManagerData {
         }
         Ok(())
     }
+    pub fn update_relics(
+        &mut self,
+        class: Class,
+        process: &Process,
+        module: &Module,
+        singleton: Class,
+    ) -> Result<(), Error> {
+        let mut buttons = vec![];
+
+        if let Ok(relic_buttons) = class.follow_fields::<u64>(singleton, process, module, &["relicSelectionScreen", "relicButtons"]) {
+            let mut fields_base = 0x20;
+            let offset = 0x8;
+
+            let items_ptr = process.read_pointer::<u64>(relic_buttons + 0x10)?;
+            let count = process.read_pointer::<u32>(items_ptr + 0x18)?;
+
+            if count == 0 { return Ok(()) }
+            
+            // TODO(eein): try to convert this items reader to be generic somehow
+            // maybe something like UnityItems<RelicButton> and have Relic Button implement how it
+            // gets and stores its own data with a UnityItem trait
+            for _index in 0..count {
+                let item_ptr = process.read_pointer::<u64>(items_ptr + fields_base)?;
+                if item_ptr == 0 { break; }
+
+                // NAME
+                let len = process.read_pointer_path::<u8>(item_ptr, &[0x188, 0xD8, 0x10])?;
+                let addr = process.read_pointer_path_without_read(item_ptr, &[0x188, 0xD8, 0x14])?;
+                
+                // Get the buffer for the name and store it
+                // TODO(eein): convert this utf8 string reader to a helper function
+                let mut buf = vec![0; (len * 2) as usize];
+                let current_read_buf = &mut buf[..(len * 2) as usize];
+                let current_read_buf = process.read_into_uninit_buf(addr, current_read_buf)?;
+                let name = std::str::from_utf8(current_read_buf).ok().unwrap();
+
+                // ENABLED 
+                // TODO(eein): rewrite this to a CSTR lookup looking for null terminator
+                let addr = process.read_pointer_path_without_read(item_ptr, &[0x1B0, 0xD8, 0x10, 0x30, 0x0])?;
+                let mut buf = [0; 16];
+                let current_read_buf = &mut buf[..16];
+                let current_read_buf = process.read_into_uninit_buf(addr, current_read_buf)?;
+                let enabled_str = std::str::from_utf8(current_read_buf).ok().unwrap();
+
+                let enabled = !matches!(enabled_str.trim(), "relic-switch-off");
+
+                buttons.push(RelicButton {
+                    name: name.to_string(),
+                    enabled
+                });
+
+                fields_base += offset;
+            }
+            self.relic_buttons = RelicButtons {
+                count,
+                buttons,
+            };
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Default, Debug)]
@@ -197,4 +261,20 @@ pub enum TitleMenuOption {
 #[derive(Default, Debug)]
 pub struct TitleMenu {
     pub selected: TitleMenuOption,
+}
+
+#[derive(Default, Debug)]
+pub struct RelicButtons {
+    pub count: u32, // 0x18
+    pub buttons: Vec<RelicButton>, // start at 0x20
+}
+
+#[derive(Default, Debug)]
+pub struct RelicButton {
+    // textfield -> m_Text -> Value
+    pub name: String,
+    // We check the on off switch state to determine if it is enabled or not.
+    // onOffSwitchImage -> m_Sprite -> m_CachedPtr -> ptr to base -> 0x0 = string
+    pub enabled: bool,
+
 }
