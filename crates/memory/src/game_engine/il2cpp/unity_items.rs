f49e1df438ff2a1987e89efb6b062c07014fc390
William Volin <me@williamvolin.com>
2024-10-03 12:32:31 -0700

    chore: cleanup docs and magic values

--- crates/memory/src/game_engine/il2cpp/unity_items.rs
+++ crates/memory/src/game_engine/il2cpp/unity_items.rs
@@ -6,17 +6,34 @@ pub struct UnityItems<T: UnityItem> {
     pub items: Vec<T>,
 }
 
+const ITEMS_OFFSET: u64 = 0x10;
+const OFFSET: u64 = 0x8;
+const ITEMS_0_INDEX_BASE: u64 = 0x20;
+const COUNT_OFFSET: u64 = 0x18;
+
+///  UnityItems are laid out in the following format:
+///  ```
+///  field -> ptr
+///    0x10 items -> ptr
+///      0x08 count -> u32
+///      0x20 item[0] -> ptr
+///      0x28 item[1] -> ptr
+///      0x30 item[x] -> ptr
+///  ```
+
 impl<T: UnityItem> UnityItems<T> {
     pub fn read(process: &Process, addr: u64) -> Result<Self, Error> {
-        const OFFSET: u64 = 0x8;
-        const ITEMS_0_INDEX_BASE: u64 = 0x20;
-
         let mut items = vec![];
         let mut fields_base = ITEMS_0_INDEX_BASE;
 
-        let items_ptr = process.read_pointer::<u64>(addr + 0x10)?;
-        let count = process.read_pointer::<u32>(items_ptr + 0x18)?;
+        // The root items pointer
+        let items_ptr = process.read_pointer::<u64>(addr + ITEMS_OFFSET)?;
 
+        // The count in the items pointer
+        let count = process.read_pointer::<u32>(items_ptr + COUNT_OFFSET)?;
+
+        // ensure neither of these is 0, or else just dummy the struct
+        // this is for performance and memory safety
         if items_ptr == 0 {
             return Ok(Self { count: 0, items });
         }
@@ -26,6 +43,7 @@ impl<T: UnityItem> UnityItems<T> {
         }
 
         for _index in 0..count {
+            // the item in the items pointer by offset
             let item_ptr = process.read_pointer::<u64>(items_ptr + fields_base)?;
 
             if item_ptr != 0 {
