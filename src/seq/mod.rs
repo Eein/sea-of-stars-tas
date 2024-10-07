24055932680c81591710b2a4a60db286accf767e
orkaboy <orkaboy@gmail.com>
2024-10-07 22:58:54 +0200

    Added temporary Seq implementation in SoS code crate

---
+++ src/seq/mod.rs
@@ -0,0 +1,33 @@
+use crate::control::SosAction;
+use crate::state::GameState;
+use seq::prelude::*;
+
+// Temp, press the confirm button for x time
+pub struct SeqConfirm {
+    timer: f64,
+    timeout: f64,
+}
+
+impl SeqConfirm {
+    pub fn create(timeout: f64) -> Box<Self> {
+        Box::new(Self {
+            timer: 0.0,
+            timeout,
+        })
+    }
+}
+
+impl Node<GameState> for SeqConfirm {
+    fn enter(&mut self, state: &mut GameState) {
+        state.gamepad.press(&SosAction::Confirm);
+    }
+
+    fn execute(&mut self, _state: &mut GameState, delta: f64) -> bool {
+        self.timer += delta;
+        self.timer >= self.timeout
+    }
+
+    fn exit(&self, state: &mut GameState) {
+        state.gamepad.release(&SosAction::Confirm);
+    }
+}
