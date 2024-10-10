28c4fb2ee6fbf06f6c6746ba3c483eee18acee41
William Volin <me@williamvolin.com>
2024-10-10 00:04:49 -0700

    feat: add level manager

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,4 +1,5 @@
 pub mod boat_manager;
+pub mod level_manager;
 pub mod memory_context;
 pub mod objects;
 pub mod player_party_manager;
@@ -10,6 +11,7 @@ use log::error;
 use crate::state::StateContext;
 
 use boat_manager::BoatManagerData;
+use level_manager::LevelManagerData;
 use memory::memory_manager::il2cpp::{UnityMemoryManagement, UnityMemoryManager};
 use memory::process::MemoryError;
 use player_party_manager::PlayerPartyManagerData;
@@ -36,6 +38,7 @@ pub struct MemoryManagers {
     pub player_party_manager: MemoryManager<PlayerPartyManagerData>,
     pub time_of_day_manager: MemoryManager<TimeOfDayManagerData>,
     pub boat_manager: MemoryManager<BoatManagerData>,
+    pub level_manager: MemoryManager<LevelManagerData>,
 }
 
 impl MemoryManagers {
@@ -45,6 +48,7 @@ impl MemoryManagers {
             self.player_party_manager.update(ctx);
             self.time_of_day_manager.update(ctx);
             self.boat_manager.update(ctx);
+            self.level_manager.update(ctx);
         }
     }
     pub fn ready_for_updates(&mut self, ctx: &StateContext) -> bool {
