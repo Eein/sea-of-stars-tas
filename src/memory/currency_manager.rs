6663151b9fbf5ecb52035b55397565e0cc2a6f50
William Volin <me@williamvolin.com>
2024-10-10 00:54:20 -0700

    chore: comment currency manager data about Unity Dicts
    chore: fmt

--- src/memory/currency_manager.rs
+++ src/memory/currency_manager.rs
@@ -37,7 +37,21 @@ impl MemoryManagerUpdate for CurrencyManagerData {
 }
 
 impl CurrencyManagerData {
-    // Currencies are in a dict, which a
+    // Currencies are in a C# Dictionary<K,V> - this is complicated to parse out of memory so we'll
+    // get the value directly.
+    // In this case I was unable to locate the currency reference and it did not seem to be
+    // following standard C# Dict protocol for Dictionary<CurrencyReference, Int>
+    // Interally on CE it looked like the following:
+    // 0x18 - _entries
+    //   0x18 - Count -> 3
+    //   0x20 - Item[0] -> Pointer to unusable (P->FFFFFFFFFFFFFFXXXX)
+    //   0x28 - Item[1] -> Pointer to unusable (P->valid looking pointer)
+    //   0x30 - Item[3] -> Currency Int (correct value in this case)
+    //   0x38 - Item[4] -> P->00000000
+    // This doesn't seem to match up to the capabilities of C# in the version used to compile the
+    // game. Possibly SOA?
+    //
+    // TODO(eein): attempt to implement UnityDict another time.
     pub fn update_currencies(&mut self, memory_context: &MemoryContext) -> Result<(), MemoryError> {
         if let Ok(currency_ptr) = memory_context.read_pointer_path::<u64>(&[0x28, 0x18]) {
             let count = memory_context.read_pointer::<u64>(currency_ptr + 0x30)?;
