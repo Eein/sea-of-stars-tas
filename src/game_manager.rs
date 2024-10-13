6cabf1a9219190aada306ac78f55dec71d4c3571
orkaboy <orkaboy@gmail.com>
2024-10-13 22:20:53 +0200

    Outlining the FSM of the game manager

--- src/game_manager.rs
+++ src/game_manager.rs
@@ -1,15 +1,25 @@
+use joystick::common::JoystickInterface;
 use seq::prelude::*;
 
 use crate::state::GameState;
 
+#[derive(Default)]
+enum GameFsm {
+    Combat,
+    #[default]
+    Route,
+}
+
 pub struct GameManager {
     sequencer: Sequencer<GameState>,
+    fsm: GameFsm,
 }
 
 impl GameManager {
     pub fn create(root: Box<dyn Node<GameState>>) -> Self {
         Self {
             sequencer: Sequencer::create(root),
+            fsm: GameFsm::default(),
         }
     }
 
@@ -20,13 +30,29 @@ impl GameManager {
     pub fn run(&mut self, context: &mut GameState) -> bool {
         let cmd = &context.memory_managers.combat_manager.data;
 
-        // TODO(orkaboy): level up screen
-        // TODO(orkaboy): actually handle combat
+        // TODO(orkaboy): detect game over?
+        // TODO(orkaboy): detect level up screen
         if cmd.encounter_active {
-            return false;
+            // Stop whatever we're doing and enter combat controller
+            context.gamepad.release_all();
+            self.fsm = GameFsm::Combat;
         }
-        // Sequencer has lower prio
-        self.sequencer.run(context)
+
+        match self.fsm {
+            GameFsm::Combat => {
+                // TODO(orkaboy): actually handle combat. For now, handle manually
+                if !cmd.encounter_active {
+                    // TODO(orkaboy): Relinquish to sequencer immediately or hold until we're sure combat isn't done
+                    // TODO(orkaboy): Signal return to sequencer?
+                    self.fsm = GameFsm::Route;
+                }
+            },
+            GameFsm::Route => {
+                // Sequencer has lower prio
+                return self.sequencer.run(context)
+            }
+        }
+        false
     }
 
     pub fn is_running(&self) -> bool {
