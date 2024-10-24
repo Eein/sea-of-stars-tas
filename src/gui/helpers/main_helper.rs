6b31c78b8f50481287dc90155490fd81158129ff
orkaboy <orkaboy@gmail.com>
2024-10-24 06:47:07 +0200

    only show title screen when its active

--- src/gui/helpers/main_helper.rs
+++ src/gui/helpers/main_helper.rs
@@ -101,71 +101,74 @@ impl MainHelper {
             cmd.combo_point_progress, cmd.combo_points, cmd.ultimate_progress
         ));
         ui.separator();
-        ui.label("Enemies");
-        egui::Grid::new("enemies")
-            .min_col_width(10.0)
-            .striped(true)
+        egui::CollapsingHeader::new("Enemies")
+            .default_open(true)
             .show(ui, |ui| {
-                ui.label("Name");
-                ui.label("Lv");
-                ui.label("Guid");
-                ui.label("Uuid");
-                ui.label("HP");
-                ui.label("Spd");
-                ui.label("Patk");
-                ui.label("Pdef");
-                ui.label("Matk");
-                ui.label("Mdef");
-                ui.label("Act");
-                ui.label("#");
-                ui.label("Locks");
-                ui.label("Mod");
-                ui.label("ModOverride");
-                ui.label("Fleshmancer");
-                ui.label("ManaSpnQty");
+                egui::Grid::new("enemies")
+                    .min_col_width(10.0)
+                    .striped(true)
+                    .show(ui, |ui| {
+                        ui.label("Name");
+                        ui.label("Lv");
+                        ui.label("Guid");
+                        ui.label("Uuid");
+                        ui.label("HP");
+                        ui.label("Spd");
+                        ui.label("Patk");
+                        ui.label("Pdef");
+                        ui.label("Matk");
+                        ui.label("Mdef");
+                        ui.label("Act");
+                        ui.label("#");
+                        ui.label("Locks");
+                        ui.label("Mod");
+                        ui.label("ModOverride");
+                        ui.label("Fleshmancer");
+                        ui.label("ManaSpnQty");
 
-                ui.end_row();
+                        ui.end_row();
 
-                for (i, enemy) in cmd.enemies.items.iter().enumerate() {
-                    ui.label(format!("NYI ({})", i));
-                    ui.label(format!("{}", enemy.level));
-                    ui.label(format!("{:.5}", enemy.guid));
-                    ui.label(format!("{:.5}", enemy.unique_id));
-                    ui.label(format!("{}/{}", enemy.current_hp, enemy.max_hp));
-                    ui.label(format!("{}", enemy.speed));
-                    ui.label(format!("{}", enemy.physical_attack));
-                    ui.label(format!("{}", enemy.physical_defense));
-                    ui.label(format!("{}", enemy.magical_attack));
-                    ui.label(format!("{}", enemy.magical_defense));
-                    ui.label(format!("{}", enemy.turns_to_action));
-                    ui.label(format!("{}", enemy.total_spell_locks));
+                        for (i, enemy) in cmd.enemies.items.iter().enumerate() {
+                            ui.label(format!("NYI ({})", i));
+                            ui.label(format!("{}", enemy.level));
+                            ui.label(format!("{:.5}", enemy.guid));
+                            ui.label(format!("{:.5}", enemy.unique_id));
+                            ui.label(format!("{}/{}", enemy.current_hp, enemy.max_hp));
+                            ui.label(format!("{}", enemy.speed));
+                            ui.label(format!("{}", enemy.physical_attack));
+                            ui.label(format!("{}", enemy.physical_defense));
+                            ui.label(format!("{}", enemy.magical_attack));
+                            ui.label(format!("{}", enemy.magical_defense));
+                            ui.label(format!("{}", enemy.turns_to_action));
+                            ui.label(format!("{}", enemy.total_spell_locks));
 
-                    ui.horizontal(|ui| {
-                        for modifier in enemy.spell_locks.items.iter() {
-                            damage_type_image(ui, modifier)
+                            ui.horizontal(|ui| {
+                                for modifier in enemy.spell_locks.items.iter() {
+                                    damage_type_image(ui, modifier)
+                                }
+                                // This is required to push the column that images dont seem to resize
+                                ui.label("");
+                            });
+
+                            ui.horizontal(|ui| {
+                                for modifier in enemy.damage_type_modifiers.items.iter() {
+                                    damage_type_image(ui, &modifier.0.key);
+                                    ui.label(format!("{}", &modifier.1.value));
+                                }
+                            });
+                            ui.horizontal(|ui| {
+                                for modifier in enemy.damage_type_modifiers_override.items.iter() {
+                                    damage_type_image(ui, &modifier.0.key);
+                                    ui.label(format!("{}", &modifier.1.value));
+                                }
+                            });
+
+                            ui.label(format!("{}", enemy.fleshmancer_minion));
+                            ui.label(format!("{}", enemy.live_mana_spawn_quantity));
+
+                            ui.end_row();
                         }
-                        // This is required to push the column that images dont seem to resize
-                        ui.label("");
                     });
-
-                    ui.horizontal(|ui| {
-                        for modifier in enemy.damage_type_modifiers.items.iter() {
-                            damage_type_image(ui, &modifier.0.key);
-                            ui.label(format!("{}", &modifier.1.value));
-                        }
-                    });
-                    ui.horizontal(|ui| {
-                        for modifier in enemy.damage_type_modifiers_override.items.iter() {
-                            damage_type_image(ui, &modifier.0.key);
-                            ui.label(format!("{}", &modifier.1.value));
-                        }
-                    });
-
-                    ui.label(format!("{}", enemy.fleshmancer_minion));
-                    ui.label(format!("{}", enemy.live_mana_spawn_quantity));
-
-                    ui.end_row();
-                }
             });
     }
 
@@ -277,51 +280,53 @@ impl GuiHelper for MainHelper {
         ui: &mut egui::Ui,
         _tab: &mut String,
     ) {
-        ui.label("TAS Option".to_string());
-
-        ui.checkbox(&mut game_state.config.konami_code, "Konami Code");
-
-        ui.separator();
-
         let mut running = false;
         if let Some(gm) = game_manager {
             running = gm.is_running();
         }
 
-        egui::ComboBox::from_label("Checkpoint")
-            .selected_text(
-                self.checkpoint
-                    .clone()
-                    .unwrap_or(String::from("New Game"))
-                    .to_string(),
-            )
-            .show_ui(ui, |ui| {
-                for checkpoint in [
-                    "New Game",
-                    "Mooncradle Intro Cavern",
-                    "Zenith Academy Dorms",
-                    "Zenith Academy Dorms 2",
-                    "Outside Forbidden Cavern",
-                    "Before Bosslug",
-                    "After Elder Mist Boss-fight",
-                ] {
-                    ui.selectable_value(
-                        &mut self.checkpoint,
-                        Some(checkpoint.to_string()),
-                        checkpoint,
-                    );
-                }
-            });
+        if !running {
+            ui.label("TAS Option".to_string());
 
-        if ui
-            .add_enabled(!running, egui::Button::new("Start TAS"))
-            .clicked()
-        {
-            let mut gm = tas::create_tas();
-            if let Some(checkpoint) = &self.checkpoint {
-                gm.advance_to_checkpoint(game_state, checkpoint);
+            ui.checkbox(&mut game_state.config.konami_code, "Konami Code");
+
+            ui.separator();
+
+            egui::ComboBox::from_label("Checkpoint")
+                .selected_text(
+                    self.checkpoint
+                        .clone()
+                        .unwrap_or(String::from("New Game"))
+                        .to_string(),
+                )
+                .show_ui(ui, |ui| {
+                    for checkpoint in [
+                        "New Game",
+                        "Mooncradle Intro Cavern",
+                        "Zenith Academy Dorms",
+                        "Zenith Academy Dorms 2",
+                        "Outside Forbidden Cavern",
+                        "Before Bosslug",
+                        "After Elder Mist Boss-fight",
+                    ] {
+                        ui.selectable_value(
+                            &mut self.checkpoint,
+                            Some(checkpoint.to_string()),
+                            checkpoint,
+                        );
+                    }
+                });
+
+            if ui
+                .add_enabled(!running, egui::Button::new("Start TAS"))
+                .clicked()
+            {
+                let mut gm = tas::create_tas();
+                if let Some(checkpoint) = &self.checkpoint {
+                    gm.advance_to_checkpoint(game_state, checkpoint);
+                }
+                *game_manager = Some(gm);
             }
-            *game_manager = Some(gm);
         }
 
         if let Some(gm) = game_manager {
@@ -342,15 +347,15 @@ impl GuiHelper for MainHelper {
 
         let cmd = &game_state.memory_managers.combat_manager.data;
         let lum = &game_state.memory_managers.level_up_manager.data;
-        //let tsmd = &game_state.memory_managers.title_sequence_manager.data;
+        let tsmd = &game_state.memory_managers.title_sequence_manager.data;
 
-        self.draw_players(game_state, ui);
         if cmd.encounter_active {
             self.draw_combat(game_state, ui);
+            self.draw_players(game_state, ui);
         } else if lum.active {
             self.draw_level_up(game_state, ui);
+        } else if tsmd.active {
+            self.draw_title(game_state, ui);
         }
-        // TODO: Only when active
-        self.draw_title(game_state, ui);
     }
 }
